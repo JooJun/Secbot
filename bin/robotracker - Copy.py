49989b7d47c97834aed5881520ce9eb68c56537f
standingpaul <standingpaul@users.noreply.github.com>
2018-03-19 22:02:19 +0200

    More attempts ad video optimisation

--- bin/robotracker - Copy.py
+++ bin/robotracker - Copy.py
@@ -2,10 +2,10 @@ from tkinter import *
 from tkinter import ttk
 import os
 import time
-#from threading import Thread
+import threading
 
 #open cv imports
-import numpy
+import numpy as np
 import cv2 as cv
 from PIL import Image, ImageTk	
 
@@ -16,6 +16,7 @@ class App:
 		
 		#Pass the parameter master to variable self.master
 		self.master = master
+		
 		#key binding definition for f key to fullscreen function
 		self.master.bind('<Key>', self.event_handler)
 		
@@ -27,9 +28,11 @@ class App:
 		# self.destructor function gets fired when the window is closed (not working)
 		# self.master.protocol('ROBOTRACKER v1.0', self.destructor)	
 		
-		#self.vs = cv.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"):
+		#self.vs = cv.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream")
 		self.vs = cv.VideoCapture('C:\\Users\\paultobias\\Downloads\\MOV_1114.mp4')		
-		
+		self.raw_image = False
+		self.current_image = False
+				
 		#Configure the master frame's grid
 		self.master.grid_rowconfigure(0, weight=1)
 		self.master.grid_rowconfigure(1, weight=1)
@@ -61,36 +64,52 @@ class App:
 		#frame 3 contains the TEXT BOX
 		self.frame3 = Frame(self.master, background="black")
 		self.frame3.grid(column = 0, row = 1, sticky='nsew')	
-		self.frame3.grid_rowconfigure(0,weight = 1)
-		self.frame3.grid_columnconfigure(0,weight = 1)
-		
-		self.scroll=Scrollbar()		
-		self.textArea = Text(self.frame3,wrap=WORD, yscrollcommand=self.scroll.set, foreground="black", background="white", width=1, height=1, state=DISABLED)		
+		self.frame3.grid_rowconfigure(0,weight = 1)		
+		self.frame3.grid_columnconfigure(0,weight = 10)
+		self.frame3.grid_columnconfigure(1,weight = 1)		
+				
+		self.textArea = Text(self.frame3,wrap=WORD, foreground="black", background="white", width=1, height=1, state=DISABLED)		
 		self.textArea.grid(column=0,row=0,sticky='nsew')
-		self.textArea.config(state=NORMAL)		
+		self.scroll=Scrollbar(self.textArea, orient = VERTICAL)
+		
+		self.textArea.config(yscrollcommand=self.scroll.set) 	
+		self.scroll.config(command=self.textArea.yview)
+		self.scroll.grid(column=1, row=0)		
 		
 		#Frame 4 spare for later use
 		self.frame4 = Frame(self.master, background="black")
 		self.frame4.grid(column = 1, row = 1, sticky='nsew')		
-		
-		self.console_fhandle = open('C:\\Users\\paultobias\\Desktop\\console.txt','r')
+		try:			
+			self.console_fhandle = open('C:\\Users\\paultobias\\Desktop\\console.txt','r')
+		except:
+			self.console_fhandle = False
 		self.console_modified = 0
+
+		self.current_size = [0,0]
+		self.previous_size = [1,1]
 		
-	def video_feed(self):				
-		ok, frame = self.vs.read()  # read frame from video stream
-		if ok:  # frame captured without any errors
-			key = cv.waitKey(5)
-			cvimage = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
-			self.current_image = Image.fromarray(cvimage)  # convert image for PIL			
-			if self.image_box.winfo_width()>1:
+	def video_feed(self):
+		if size self.image_box.winfo_width()>1:
+			if self.image_box.winfo_height() < int((self.image_box.winfo_width()*0.5625)): 
+					self.current_size = ([int(self.image_box.winfo_height()*1.77777),(self.image_box.winfo_height())])					
+				else:
+					self.current_size = ([self.image_box.winfo_width(),int((self.image_box.winfo_width()*0.5625))])	
+			
+		ok, frame = self.vs.read()
+		if ok:			
+			#key = cv.waitKey(100)
+			self.raw_image = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)
+			self.current_image = Image.fromarray(self.raw_image)
+			
+			if size self.image_box.winfo_width()>1 && self.current_size != self.previous_size:
 				if self.image_box.winfo_height() < int((self.image_box.winfo_width()*0.5625)): 
 					self.current_image=self.current_image.resize([int(self.image_box.winfo_height()*1.77777),(self.image_box.winfo_height())])					
 				else:
 					self.current_image=self.current_image.resize([self.image_box.winfo_width(),int((self.image_box.winfo_width()*0.5625))])					
-			imgtk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter 						
-			self.image_box.imgtk = imgtk  # anchor imgtk so as not be deleted by garbage-collector		
-			self.image_box.config(image=imgtk)  # show the image			
-		self.master.after(3, self.video_feed)  # call the same function after 30 milliseconds
+			imagetk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter 			
+			self.image_box.imgtk = imagetk  # anchor imgtk so as not be deleted by garbage-collector		
+			self.image_box.config(image=imagetk)  # show the image	
+		self.master.after(3, self.video_feed)		
 	
 	def event_handler(self,event=None):			
 		if event.char == 'f' or event.keysym == 'Escape' or event.num == 1:
@@ -101,7 +120,11 @@ class App:
 			if self.fullscreen_status == False:				
 				self.frame1.grid(columnspan = 2,rowspan=2)
 				self.frame1.lift()
-				self.current_image= self.current_image.resize([self.master.winfo_width(),int((self.master.winfo_width()*0.5625))])
+				# self.current_image= self.current_image.resize([self.master.winfo_width(),int((self.master.winfo_width()*0.5625))])
+				if self.image_box.winfo_height() < int((self.image_box.winfo_width()*0.5625)): 
+					self.current_image=self.current_image.resize([int(self.image_box.winfo_height()*1.77777),(self.image_box.winfo_height())])					
+				else:
+					self.current_image=self.current_image.resize([self.image_box.winfo_width(),int((self.image_box.winfo_width()*0.5625))])
 				self.fullscreen_status = True			
 			else:
 				self.frame1.grid(columnspan = 1,rowspan=1)
@@ -109,18 +132,28 @@ class App:
 		if event.keysym == 'Escape':
 			if self.fullscreen_status == True:
 				self.frame1.grid(columnspan = 1,rowspan=1)
-				self.fullscreen_status = False		
-				
-	# def write_to_console(self):		
-		# self.textArea.insert(INSERT,text.read())		
-		# self.textArea.config(state=DISABLED)
-		# text.close()	
+				self.fullscreen_status = False	
 		
 	def write_to_console(self):
-		if self.console_modified != time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime('C:\\Users\\paultobias\\Desktop\\console.txt'))):
-			self.console_list = []
-			for line in	self.console_fhandle:
-				self.console_list.append(line)
+		if self.console_fhandle:
+			if self.console_modified != time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime('C:\\Users\\paultobias\\Desktop\\console.txt'))):
+				self.console_list = []
+				for line in	self.console_fhandle:
+					self.console_list.append(line)				
+				for item in self.console_list:
+					self.textArea.config(state=NORMAL)
+					self.textArea.insert(END,item)
+					self.textArea.config(state=DISABLED)
+					self.textArea.see("end")
+		else:
+			self.textArea.config(state=NORMAL)
+			self.textArea.delete('1.0', END)
+			self.textArea.insert(INSERT,"No console data found")
+			self.textArea.config(state=DISABLED)
+			try:			
+				self.console_fhandle = open('C:\\Users\\paultobias\\Desktop\\console.txt','r')
+			except:
+				pass
 		self.master.after(1000, self.write_to_console)
 	
 #resolution - height,width	
@@ -132,7 +165,7 @@ root = Tk()
 app = App(root,res)
 #call videofeed and write_to_console functions from app class
 app.video_feed()
-app.write_to_console()
+#app.write_to_console()
 
 try:
 #start tkinter mainloop
--- bin/robotracker.py
+++ bin/robotracker.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import os
 import time
-#from threading import Thread
+import threading
 
 #open cv imports
 import numpy as np
@@ -23,15 +23,14 @@ class App:
 		self.fullscreen_status = False		
 		
 		#set the titlebar text
-		self.master.title("ROBOTRACKER v1.0")
+		self.master.title("ROBOTRACKER v1.0")	
 		
-		# self.destructor function gets fired when the window is closed (not working)
-		# self.master.protocol('ROBOTRACKER v1.0', self.destructor)	
+		self.vs = cv.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream")		
+		#self.vs = cv.VideoCapture('C:\\Users\\paultobias\\Downloads\\MOV_1114.mp4')
 		
-		#self.vs = cv.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"):
-		self.vs = cv.VideoCapture('C:\\Users\\paultobias\\Downloads\\MOV_1114.mp4')		
-		# self.vs.set(CV_CAP_PROP_FPS , 60)
-		
+		self.raw_image = False
+		self.current_image = False
+				
 		#Configure the master frame's grid
 		self.master.grid_rowconfigure(0, weight=1)
 		self.master.grid_rowconfigure(1, weight=1)
@@ -62,83 +61,76 @@ class App:
 		
 		#frame 3 contains the TEXT BOX
 		self.frame3 = Frame(self.master, background="black")
-		self.frame3.grid(column = 0, row = 1, sticky='nsew')	
-		self.frame3.grid_rowconfigure(0,weight = 1)		
-		self.frame3.grid_columnconfigure(0,weight = 10)
-		self.frame3.grid_columnconfigure(1,weight = 1)		
+		self.frame3.grid(column = 0, row = 1, sticky='nsew')					
 				
 		self.textArea = Text(self.frame3,wrap=WORD, foreground="black", background="white", width=1, height=1, state=DISABLED)		
-		self.textArea.grid(column=0,row=0,sticky='nsew')
+		# self.textArea.grid(sticky='nsew')
+	
 		self.scroll=Scrollbar(self.textArea, orient = VERTICAL)
 		
 		self.textArea.config(yscrollcommand=self.scroll.set) 	
-		self.scroll.config(command=self.textArea.yview)
-		self.scroll.grid(column=1, row=0)
-		
-		
-		#Frame 4 spare for later use
-		self.frame4 = Frame(self.master, background="black")
-		self.frame4.grid(column = 1, row = 1, sticky='nsew')		
+		# self.scroll.config(command=self.textArea.yview)	
+
+		#Open the console file
 		try:			
 			self.console_fhandle = open('C:\\Users\\paultobias\\Desktop\\console.txt','r')
 		except:
 			self.console_fhandle = False
-		self.console_modified = 0		
+			
+		self.console_modified = 0
 		
-	def video_feed(self):				
-		# ok, frame = self.vs.read()  # read frame from video stream
-		# #print(int(self.image_box.winfo_height()*1.77777),int(self.image_box.winfo_height()))
-		# if ok:  # frame captured without any errors
-			# key = cv.waitKey(1)
-			# cvimage = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
-			# self.current_image = Image.fromarray(cvimage)  # convert image for PIL	
-			# if self.image_box.winfo_width()>1:			
-				# if self.image_box.winfo_height() < int((self.image_box.winfo_width()*0.5625)): 
-					# self.current_image=self.current_image.resize([int(self.image_box.winfo_height()*1.77777),(self.image_box.winfo_height())])					
-				# else:
-					# self.current_image=self.current_image.resize([self.image_box.winfo_width(),int((self.image_box.winfo_width()*0.5625))])					
-			# imgtk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter 						
-			# self.image_box.imgtk = imgtk  # anchor imgtk so as not be deleted by garbage-collector		
-			# self.image_box.config(image=imgtk)  # show the image			
-		# self.master.after(3, self.video_feed)  # call the same function after 30 milliseconds
+		#Frame 4 spare for later use
+		self.frame4 = Frame(self.master, background="black")
+		self.frame4.grid(column = 1, row = 1, sticky='nsew')	
+		
+		#For video feed
+		self.image_box_previous_size = [0,0]
+		
+	def resize(self,size):
+		# if size[0]> 960 or size[1] > 540: 
+		# if size[0]> 1280 or size[1] > 720:
+			#self.current_image=self.current_image.resize([960,540])
+		#else:
+		self.current_image=self.current_image.resize(size)
+		self.image_box_previous_size = (self.image_box.winfo_width(), self.image_box.winfo_height())
+			
+	def video_feed(self):
+		if self.image_box.winfo_width()>1 and self.image_box_previous_size != (self.image_box.winfo_width(), self.image_box.winfo_height()):			
+			if self.image_box.winfo_height() < int((self.image_box.winfo_width()*0.5625)): 
+				self.current_size = ([int(self.image_box.winfo_height()*1.77777),(self.image_box.winfo_height())])	
+
+			else:
+				self.current_size = ([self.image_box.winfo_width(),int((self.image_box.winfo_width()*0.5625))])
 				
 		ok, frame = self.vs.read()
 		if ok:			
-			key = cv.waitKey(5)
-			image = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)			
-			#cv.imshow('frame',image)
-			self.current_image = Image.fromarray(image)
-			if self.image_box.winfo_width()>1:
-				self.current_image = self.current_image.resize([self.image_box.winfo_width(),int((self.image_box.winfo_width()*0.5625))])			
-			imagetk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter 			
+			#key = cv.waitKey(10)			
+			cvimage = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)			
+			self.current_image = Image.fromarray(frame)#PIL stuff ?			
+			if self.image_box.winfo_width()>1:				
+				self.resize(self.current_size)				
+			imagetk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter 
+			self.image_box.imgtk = imagetk 	
 			self.image_box.imgtk = imagetk  # anchor imgtk so as not be deleted by garbage-collector		
-			self.image_box.config(image=imagetk)  # show the image			
-		self.master.after(10, self.video_feed)
+			self.image_box.config(image=imagetk)  # show the image	
+		self.master.after(8, self.video_feed)			
 	
 	def event_handler(self,event=None):			
 		if event.char == 'f' or event.keysym == 'Escape' or event.num == 1:
-			self.fullscreen(event)		
+			self.fullscreen(event)	
+		if event.char == 'q':
+			pass			
 			
 	def fullscreen(self,event=None):
-		if event.char == 'f' or event.num == 1:
-			if self.fullscreen_status == False:				
+		if event.char == 'f' or event.num == 1 or event.keysym == 'Escape':
+			if self.fullscreen_status == False and (event.char == 'f' or event.num == 1):					
 				self.frame1.grid(columnspan = 2,rowspan=2)
-				self.frame1.lift()
-				self.current_image= self.current_image.resize([self.master.winfo_width(),int((self.master.winfo_width()*0.5625))])
+				self.frame1.lift()	
 				self.fullscreen_status = True			
 			else:
 				self.frame1.grid(columnspan = 1,rowspan=1)
-				self.fullscreen_status = False
-		if event.keysym == 'Escape':
-			if self.fullscreen_status == True:
-				self.frame1.grid(columnspan = 1,rowspan=1)
 				self.fullscreen_status = False		
 				
-	# def write_to_console(self):		
-		# self.textArea.insert(INSERT,text.read())		
-		# self.textArea.config(state=DISABLED)
-		# text.close()	
-		
 	def write_to_console(self):
 		if self.console_fhandle:
 			if self.console_modified != time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime('C:\\Users\\paultobias\\Desktop\\console.txt'))):
@@ -162,7 +154,7 @@ class App:
 		self.master.after(1000, self.write_to_console)
 	
 #resolution - height,width	
-res = (500,900)
+res = (480,600)
 #create tkinter root
 root = Tk()
 
