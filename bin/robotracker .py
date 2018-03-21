700fa8c66eaad25e6ab5572eb418a4e2e7d0d095
standingpaul <standingpaul@users.noreply.github.com>
2018-03-21 17:51:50 +0200

    several changes for console

--- bin/robotracker .py
+++ bin/robotracker .py
@@ -20,9 +20,7 @@ class App:
 		root.iconbitmap('c:\Python34\DLLs\py.ico')		
 		
 		#Pass the parameter master to variable self.master
-		self.master = master
-		
-		
+		self.master = master		
 		
 		#key binding definition for f key to fullscreen function
 		self.master.bind('<Key>', self.event_handler)
@@ -31,7 +29,7 @@ class App:
 		self.fullscreen_status = False		
 		
 		#set the titlebar text
-		self.master.title("ROBOTRACKER v1.0")			
+		self.master.title("WARDEN GUI")			
 		
 		##Set the video source
 		#self.vs = cv.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp?real_stream")
@@ -55,11 +53,11 @@ class App:
 		self.frame1.grid_rowconfigure(0,weight = 1)
 		self.frame1.grid_columnconfigure(0,weight = 1)
 		
-		self.image_box = Label(self.frame1,width=self.frame1.winfo_width(),height=self.frame1.winfo_height(),background="black")
-		self.image_box.grid(sticky='nsew')
+		self.video_frame = Label(self.frame1,width=self.frame1.winfo_width(),height=self.frame1.winfo_height(),background="black")
+		self.video_frame.grid(sticky='nsew')
 		
 		#bind double mouse click to the image_box
-		self.image_box.bind('<Double-Button>', self.event_handler)
+		self.video_frame.bind('<Double-Button>', self.event_handler)
 		
 		#Frame 2 JPEG?
 		self.frame2 = Frame(self.master, background="black")
@@ -67,7 +65,11 @@ class App:
 		self.frame2.grid_rowconfigure(0,weight = 0)
 		self.frame2.grid_columnconfigure(0,weight = 0)			
 		
+		self.image_frame = Label(self.frame2,width=self.frame2.winfo_width(),height=self.frame2.winfo_height(),background="black")
+		self.image_frame.grid(sticky = 'nsew')
+		
 		# self.img = ImageTk.PhotoImage(Image.open('C:\\Users\\paultobias\\Documents\\GitHub\\Secbot\\bin\\pic.jpg'))
+		#self.img = ImageTk.PhotoImage(Image.open('pic.jpg'))
 		# self.pic_box = Canvas(self.frame2,background="black")
 		# # self.pic_box.gridCanvas_rowconfigure(0,weight = 0)
 		# # self.pic_box.grid_columnconfigure(0,weight = 0)
@@ -93,43 +95,47 @@ class App:
 		
 		self.console_fhandle = False				
 		self.console_modified_time = 0
+		self.console_file_path = '/home/pi/console.txt'
+		#Check remote console file exists
+		self.console_file_exists = False
 		
 		#Frame 4 spare for later use
 		self.frame4 = Frame(self.master, background="black")
 		self.frame4.grid(column = 1, row = 1, sticky='nsew')	
 		
-		#For video feed, this is set to compare if self.image_box has changed size triggering a resize of the video picture
-		self.image_box_previous_size = [0,0]
+	def image_update(self):
+		imagetk = ImageTk.PhotoImage(Image.open('pic.jpg'))
+		self.image_frame.config(image=imagetk)
+		self.master.after(2000, self.image_update)
 		
-			
 	def video_feed(self):		
 		frame = self.vs.read()	#read the next frame 
 		
-		##resize the image if self.image_box has changed size
-		if self.image_box.winfo_width()>1 and self.image_box_previous_size != (self.image_box.winfo_width(), self.image_box.winfo_height()):			
-			if self.image_box.winfo_height() < int((self.image_box.winfo_width()*0.5625)): 
+		##resize the image if self.video_frame has changed size
+		if self.video_frame.winfo_width()>1:	
+			if self.video_frame.winfo_height() < int((self.video_frame.winfo_width()*0.5625)): 
 				frame = imutils.resize(frame 
-					,width=int(self.image_box.winfo_height()*1.77777) #image width
-					,height=self.image_box.winfo_height()#image height
-					) 
-				self.image_box_previous_size = [
-					int(self.image_box.winfo_height()*1.77777)#set new value for 'previous' width
-					,self.image_box.winfo_height()#set new value for 'previous' height
+					,width=int(self.video_frame.winfo_height()*1.77777) #image width
+					,height=self.video_frame.winfo_height()#image height
+					) 				
+				self.video_frame_previous_size = [
+					int(self.video_frame.winfo_height()*1.77777)#set new value for 'previous' width
+					,self.video_frame.winfo_height()#set new value for 'previous' height
 					]
 			else:
 				frame = imutils.resize(frame
-					,width=self.image_box.winfo_width()	#image width
-					,height=int((self.image_box.winfo_width()*0.5625)))	#image height
-				self.image_box_previous_size = [
-					self.image_box.winfo_width()#set new value for 'previous' width	
-					,int((self.image_box.winfo_width()*0.5625))#set new value for 'previous' height
-					]	
+					,width=self.video_frame.winfo_width()	#image width
+					,height=int((self.video_frame.winfo_width()*0.5625)))	#image height
+				self.video_frame_previous_size = [
+					self.video_frame.winfo_width()#set new value for 'previous' width	
+					,int((self.video_frame.winfo_width()*0.5625))#set new value for 'previous' height
+					]			
 		##process the raw frame
 		cvimage = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)	#colours picture correctly		
 		current_image = Image.fromarray(cvimage)	#Something to do with PIL, processes the matrix array			
 		imagetk = ImageTk.PhotoImage(image=current_image)  # convert image for tkinter
-		self.image_box.imgtk = imagetk  # stops garbage collection		
-		self.image_box.config(image=imagetk)  # show the image in image_box			
+		self.video_frame.imgtk = imagetk  # stops garbage collection		
+		self.video_frame.config(image=imagetk)  # show the image in image_box			
 		self.master.after(50, self.video_feed)# cause the function to be called after X milliseconds		
 	
 	def event_handler(self,event=None):		
@@ -149,26 +155,33 @@ class App:
 				self.fullscreen_status = False		
 				
 	def write_to_console(self):
-		# try:
-			# #ftp_client=self.ssh.open_sftp()
-			# #ftp_client.get('/home/pi/console.txt','C:\\Users\\paultobias\\Documents\\GitHub\\Secbot\\bin\\console.txt')
-			# #fhandle = ftp_client.open('/home/pi/console.txt')
-			# #ftp_client.close()
-			# #self.console_fhandle = open('/home/pi/console.txt','r')
-		# except:
-			# pass
-		if self.console_fhandle:	
-			modifiedtime = self.ftp_client.stat('/home/pi/console.txt').st_mtime			
-			if modifiedtime != self.console_modified_time:			
-				self.console_list = []
-				for line in	self.console_fhandle:
-					self.console_list.append(line)				
-				for item in self.console_list:
-					self.textArea.config(state=NORMAL)
+		#Check if the file exists
+		try:
+			self.console_file_exists = self.ftp_client.stat(self.console_file_path)
+		except:
+			self.console_file_exists = False
+			
+			
+		#check if the file and file handle exists
+		if self.console_file_exists:						
+			modifiedtime = self.ftp_client.stat(self.console_file_path).st_mtime				
+			if modifiedtime != self.console_modified_time:	
+				file = self.ftp_client.open(self.console_file_path)
+				console_list = []								
+				for line in	file:					
+					console_list.append(line)
+					
+				self.textArea.config(state=NORMAL)
+				self.textArea.delete('1.0', END)
+				self.textArea.config(state=DISABLED)
+				
+				for item in console_list:
+					self.textArea.config(state=NORMAL)					
 					self.textArea.insert(END,item)
 					self.textArea.config(state=DISABLED)
 					self.textArea.see("end")
 				self.console_modified_time = modifiedtime
+				file.close()
 		else:
 			self.textArea.config(state=NORMAL)
 			self.textArea.delete('1.0', END)
@@ -179,7 +192,9 @@ class App:
 				self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 				self.ssh.connect('192.168.1.176', username='pi', password='raspberry')		
 				self.ftp_client=self.ssh.open_sftp()
-				self.console_fhandle = self.ftp_client.open('/home/pi/console.txt')
+				self.console_fhandle = self.ftp_client.open(self.console_file_path)
+				if self.console_fhandle:
+					self.console_file_exists = True
 			except:
 				pass
 		self.master.after(1000, self.write_to_console)
@@ -192,10 +207,12 @@ if __name__ == '__main__':
 	root = Tk()
 	#create instance of app class
 	app = App(root,res)
-	# #call videofeed and write_to_console functions from app class
-	app.video_feed()
-	app.write_to_console()
-	#start tkinter mainloop
-	root.mainloop()
 	
+	# #call the looping functions from the instance
+	app.video_feed()
+	#app.image_update()
+	app.write_to_console()
+	
+	#start tkinter mainloop
+	root.mainloop()	
 app.vs.stop()
