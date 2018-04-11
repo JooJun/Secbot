9ca73d7011038fd4b9d57845e14c05f3c8101209
standingpaul <standingpaul@users.noreply.github.com>
2018-04-11 21:41:35 +0300

    making major changes to SSH connection checks

--- gui/backup_versions/wardenGUI - Paul working on.py
+++ gui/backup_versions/wardenGUI - Paul working on.py
@@ -62,9 +62,9 @@ class App:
 		#self.master.minsize(res[1],res[0])                             
 		self.master.geometry(str(res[1])+"x"+str(res[0]))
 		
-	#####Set 4 frames and put them into the grid (grid contains 4 equal boxes)#####
+		#####Set 4 frames and put them into the grid (grid contains 4 equal boxes)#####
 		
-	###Frame 1 will hold the VIDEO STREAM###
+		###Frame 1 will hold the VIDEO STREAM###
 		self.frame1 = Label(self.master, background="grey")
 		self.frame1.grid(column = 0, row = 0, sticky='nsew')
 		self.frame1.grid_rowconfigure(0,weight = 1)
@@ -79,7 +79,7 @@ class App:
 		#Picture to display when no video present
 		self.vid_dis_img_path = '{0}/vid_disconnect.jpg'.format(config['content_folder'])				
 		
-	###Frame 2 SWITCH / CONTROL PANEL SECTION###		
+		###Frame 2 SWITCH / CONTROL PANEL SECTION###		
 		
 		self.frame2 = Frame(self.master, background='grey')
 		self.frame2.grid(column = 1, row = 0,sticky='nsew') 
@@ -101,10 +101,11 @@ class App:
 		self.frame2_background_label = Label(self.frame2, image=self.switchpanel_background_img)
 		self.frame2_background_label.place(x=0, y=0, relwidth=1, relheight=1)		
 		
-		data_send['control_status'] = 'Manual'		
+		data_send['control_status'] = 'Manual'
+		
 				
 		
-	###frame 3 contains the CONSOLE### (Would like to add scrollbar but doesn't work so far)
+		###frame 3 contains the CONSOLE###
 		self.frame3 = Frame(self.master, background="black")
 		self.frame3.grid(column = 0, row = 1, sticky='nsew')    
 		self.frame3.grid_rowconfigure(0,weight = 1)
@@ -113,13 +114,20 @@ class App:
 						
 		self.textArea = Text(self.frame3,wrap=WORD, foreground="black", background="white", width=1, height=1, state=DISABLED)          
 		self.textArea.grid(sticky='nsew')
+
+		#Scrollbar (not working)
+		#self.scroll=Scrollbar(self.textArea, orient = VERTICAL)
+		# self.scroll=Scrollbar(self.textArea)
+		# self.scroll.grid(column=1, row=0, sticky='nsew')
+		# self.textArea.config(yscrollcommand=self.scroll.set)  
+		# self.scroll.config(command=self.textArea.yview)                       
 		
 		self.console_fhandle = False    
 		self.console_file_exists = False
 		self.console_modified_time = 0
 		self.console_file_path = config['console_file_path']	
 
-	###Frame 4 DEPTHMAP###
+		###Frame 4 DEPTHMAP###
 		self.frame4 = Frame(self.master, background="black")
 		self.frame4.grid(column = 1, row = 1, sticky='nsew')    
 		self.frame4.grid_rowconfigure(0,weight = 1)
@@ -144,7 +152,7 @@ class App:
 		self.video_ready = False                
 		self.ssh_ready = False          
 		
-	#Start threads:-
+		#Start threads:-
 		
 		#Console thread
 		self.console_thread = threading.Thread(target=self.write_to_console,args=())
@@ -167,16 +175,15 @@ class App:
 		self.conn_thread.start()        
 		
 		#Update the map thread
-		self.depmap_thread = threading.Thread(target=self.depmap_update,args=())
-		self.depmap_thread.daemon = True
-		self.depmap_thread.start()                 
+		self.img_thread = threading.Thread(target=self.image_update,args=())
+		self.img_thread.daemon = True
+		self.img_thread.start()                 
 		
 		self.console_list = []
 		self.modifiedtime = 0
 		
-		self.authProblem = False			
-	
-			
+		self.authProblem = False	
+		
 	def conn_ready(self):                   
 		while True:     
 			try:
@@ -188,7 +195,7 @@ class App:
 			except:
 				pi = ''                 
 			if 'Reply from '+self.pi_ip in pi:  
-				self.pi_conn_ready = True				
+				self.pi_conn_ready = True
 			else:
 				self.pi_conn_ready = False      
 				self.ssh_ready = False                          
@@ -198,72 +205,64 @@ class App:
 			else:
 				#print("ping failed, setting video connection and video ready to false")
 				self.video_conn_ready = False
-				self.video_ready = False  
-			time.sleep(1)	
-	
-	def ssh_connect(self,file):	
-		#Try the ls to confirm if ssh connection is active
-		ssh = False
-		try:                    
-			ssh = self.ssh.exec_command('ls', timeout=5)
-		except Exception as msg:
-			pass
-			return False	
+				self.video_ready = False                        
+			time.sleep(3)
 					
-		if self.ssh_ready == False and self.pi_conn_ready == True:                              
-			if self.authProblem != True:
-				try:
-					self.ssh.connect(self.pi_ip, username=self.pi_username, password=self.pi_password)
-					connected = True
-				except (paramiko.ssh_exception.AuthenticationException, socket.error) as msg:
-					if str(msg) == 'Authentication failed.':
-						self.authProblem = True
-					self.ssh.close()
-					connected = False                                               
-						
-				if connected == True:
-					try:
-						self.ftp_client=self.ssh.open_sftp()
-						self.ssh_ready = True   
-
-					except (paramiko.ssh_exception, socket.error) as msg:
-						# print (msg)
-						pass
-				else:                   
-					self.ssh_ready = False                                          
-
-		if self.ssh_ready == True and self.console_file_exists == False:
-			#print("ssh ready and file doesnt exist")
-			#print("should be attempting to open the file again")                           
-			try:                                            
-				self.console_fhandle = self.ftp_client.open(self.console_file_path)
-				self.console_file_exists = True
-				#print ("1")
-			except Exception as msg:
-				# print(msg)
-				pass
-					
-			if self.console_file_exists == True:                                                                                    
-				self.textArea.config(state=NORMAL)      
-				self.textArea.delete('1.0', END)
-				self.textArea.config(state=DISABLED)
-				try:
-					modifiedtime = self.ftp_client.stat(self.console_file_path).st_mtime                                            
-				except Exception as msg:
-					modifiedtime = False
-					self.console_file_exists = False	  
+	def video_feed_initialiser(self):
+		if not self.video_ready:
+			#print("video feed initilaiser has recognised that self.video_ready is false")
+			if self.video_conn_ready:
+				#print("attempting to reconnect the video connection")
+				self.vs = WebcamVideoStream(src="rtsp://"+self.cam_ip+":554/user=admin&password=&channel=1&stream=0.sdp?real_stream").start()              
+				if str(self.vs.read()) != 'None':       
+					#print("setting video_ready to true (self.vs is not none)")
+					self.video_ready = True                                 
+		self.master.after(1000, self.video_feed_initialiser)    
 	 
-	def depmap_update(self):
-		if self.img_file_exists == False:  
-			self.ssh_ready = self.ssh_connect('depth_map')
-			if self.ssh_ready == True:                         
+	def image_update(self):
+		if self.img_file_exists == False:
+			# # print("ssh ready? = ",self.ssh_ready)
+			# # print("File exists marked as false, at the top")
+			# self.img_modified_time = 0
+			# try:   
+				# ssh = self.ssh.exec_command('ls', timeout=5)
+			# except Exception as msg:
+				# #print (msg)
+				# pass
+			# if self.ssh_ready == False and self.pi_conn_ready == True:                              
+				# if self.authProblem != True:
+					# #print("ssh connect called")
+					# try:
+						# self.ssh.connect(self.pi_ip, username=self.pi_username, password=self.pi_password)
+						# connected = True
+					# except (paramiko.ssh_exception.AuthenticationException, socket.error) as msg:
+						# if str(msg) == 'Authentication failed.':
+							# self.authProblem = True
+						# self.ssh.close()
+						# connected = False                                               
+							
+					# if connected == True:
+						# try:
+							# print('sftp')
+							# self.ftp_client=self.ssh.open_sftp()
+							# print('sftp open')
+							# self.ssh_ready = True   
+
+						# except (paramiko.ssh_exception, socket.error) as msg:
+							# pass
+					# else:                   
+						# self.ssh_ready = False                                          
+
+			if self.ssh_ready == True and self.img_file_exists == False:
+				# print("ssh ready and file doesnt exist")
+				# print("should be attempting to open the file again")                           
 				try:                                            
 					self.ftp_client.get(self.depthmap_img_remote, self.depthmap_img_local)
 					self.img_file_exists = True
-
-				except (paramiko.ssh_exception.AuthenticationException, socket.error) as msg:
-					pass
-								
+					# print ("1")
+				except Exception as msg:
+					# print(msg)
+					pass								
 				if self.img_file_exists == True: 
 					try:
 						self.img_modified_time = self.ftp_client.stat(self.depthmap_img_remote).st_mtime                                            
@@ -293,19 +292,8 @@ class App:
 				self.img = ImageTk.PhotoImage(self.raw_img)             
 				self.image_frame.config(image=self.img) 
 
-		self.master.after(1000, self.depmap_update)
-	
-	def video_feed_initialiser(self):
-		if not self.video_ready:
-			#print("video feed initilaiser has recognised that self.video_ready is false")
-			if self.video_conn_ready:
-				#print("attempting to reconnect the video connection")
-				self.vs = WebcamVideoStream(src="rtsp://"+self.cam_ip+":554/user=admin&password=&channel=1&stream=0.sdp?real_stream").start()              
-				if str(self.vs.read()) != 'None':       
-					#print("setting video_ready to true (self.vs is not none)")
-					self.video_ready = True  
-		self.master.after(1000, self.video_feed_initialiser)  
-	
+		self.master.after(1000, self.image_update)
+			
 	def video_feed(self):           
 		if self.video_ready:
 			try:
@@ -338,8 +326,13 @@ class App:
 					self.vid_dis_raw_img = self.vid_dis_raw_img.resize((self.video_frame.winfo_width(),self.video_frame.winfo_height()), Image.ANTIALIAS)
 				self.vid_dis_img = ImageTk.PhotoImage(self.vid_dis_raw_img)                     
 				self.video_frame.config(image=self.vid_dis_img)                 
-		self.master.after(50, self.video_feed)# cause the function to be called after X milliseconds			
-		
+		self.master.after(50, self.video_feed)# cause the function to be called after X milliseconds
+			
+	def event_handler(self,event=None):             
+		if event.char == 'f' or event.keysym == 'Escape' or event.num == 1:
+			self.fullscreen(event)  
+		if event.char == 'q':
+			pass 	
 	
 	def data_exchange(self):
 		pass
@@ -370,7 +363,16 @@ class App:
 				file.truncate()
 				file.write(data_send)
 				file.close()
-	       
+					
+	def fullscreen(self,event=None):
+		if event.char == 'f' or event.num == 1 or event.keysym == 'Escape':
+			if self.fullscreen_status == False and (event.char == 'f' or event.num == 1):                                   
+				self.frame1.grid(columnspan = 2,rowspan=2)
+				self.frame1.lift()      
+				self.fullscreen_status = True                   
+			else:
+				self.frame1.grid(columnspan = 1,rowspan=1)
+				self.fullscreen_status = False                  
 					
 	def write_to_console(self):
 		if self.console_file_exists == False:
@@ -383,7 +385,6 @@ class App:
 			self.modifiedtime = 0
 			self.console_count = 0  
 			self.console_list = []  
-		################################################################################################
 			try:                    
 				ssh = self.ssh.exec_command('ls', timeout=5)
 			except Exception as msg:
@@ -458,24 +459,7 @@ class App:
 			else:
 				#print("console file doesnt exist line 330")
 				self.console_file_exists = False
-				self.ssh
-		self.master.after(1000, self.write_to_console)      
-
-	def fullscreen(self,event=None):
-		if event.char == 'f' or event.num == 1 or event.keysym == 'Escape':
-			if self.fullscreen_status == False and (event.char == 'f' or event.num == 1):                                   
-				self.frame1.grid(columnspan = 2,rowspan=2)
-				self.frame1.lift()      
-				self.fullscreen_status = True                   
-			else:
-				self.frame1.grid(columnspan = 1,rowspan=1)
-				self.fullscreen_status = False   
-
-	def event_handler(self,event=None):             
-		if event.char == 'f' or event.keysym == 'Escape' or event.num == 1:
-			self.fullscreen(event)  
-		if event.char == 'q':
-			pass 
+		self.master.after(1000, self.write_to_console)          
                 
 #Opening resolution - height,width    
 res = (int(config['resolution_width']),int(config['resolution_height']))
