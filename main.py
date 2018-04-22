3c9005952ba98b7e8364a21d4dca2b18966caad8
JooJun <36189099+JooJun@users.noreply.github.com>
2018-04-22 21:52:07 +0300

    Changed main.py

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ import bin.controller as controller
 # Autonomous movement imports
 import bin.autonomous as auto
 
-# Depthmap imports
-import tensorflow as tf
-import bin.depthmap as depthmap
-import lib.fcrn.tensorflow.models as fcrn_model
-
 # Open and clear log file
 if os.path.exists('files/main.log'):
     open('files/main.log', 'w').close()
@@ -31,48 +26,6 @@ logging.basicConfig(filename='files/main.log', level=logging.DEBUG)
 ########################################################################
 logging.info('Set up...')
 
-logging.info('Warming up...')
-start = timer()
-
-# Tensorflow variables 
-#width = 640
-#width = 384
-width = 192
-#height = 360
-#height = 216
-height = 108
-channels = 3
-batch = 1
-model_path = '/home/pi/Devel/secbot/lib/fcrn/models/NYU_FCRN.ckpt'
-#model_path = r'C:\Coding\Secbot\lib\fcrn\models\NYU_FCRN.ckpt'
-
-# Initializing Tensorflow and run startups
-# Placeholder for input image
-input_placeholder = tf.placeholder(tf.float32, shape=(None, height, width, channels))
-
-# Construct network
-network = fcrn_model.ResNet50UpProj({'data': input_placeholder}, batch, 1, False)
-
-# Start tensorflow session
-session = tf.Session()
-
-# Start tensorflow checkpoint 
-saver = tf.train.Saver()
-saver.restore(session, model_path)
-
-end = timer()
-logging.info('Time taken -tf : {}'.format(end-start))
-
-# Warmup Tensorflow
-for x in range(1, 2):
-    start = timer()
-    logging.info('{0} run'.format(x))
-    depthmap.depthmap_func(network, session, input_placeholder)
-    end = timer()
-    logging.info('Time taken -cam : {}'.format(end-start))
-
-logging.info('Warming up... Done!')
-
 while mode:
 
     if mode == 1:
@@ -81,7 +34,7 @@ while mode:
 
     if mode == 2:
         logging.info('Starting autonomous control...')
-        mode = auto.autonomous_func(mode, network, session, input_placeholder)
+        mode = auto.autonomous_func(mode)
 
     if not mode:
         break
